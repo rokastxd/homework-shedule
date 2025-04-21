0ce46ef10502fa546de2e8d4466eca1e2b309673
RokastXD <rok4st@gmail.com>
2025-04-21 23:50:12 +0300

    delete unused schema

--- src/server/db/schema.ts
+++ src/server/db/schema.ts
@@ -24,27 +24,6 @@ export * from './schema/disciplines'
  * @see https://orm.drizzle.team/docs/goodies#multi-project-schema
  */
 
-export const posts = createTable(
-    'post',
-    {
-        id: integer('id').primaryKey().generatedByDefaultAsIdentity(),
-        name: varchar('name', { length: 256 }),
-        createdById: varchar('created_by', { length: 255 })
-            .notNull()
-            .references(() => users.id),
-        createdAt: timestamp('created_at', { withTimezone: true })
-            .default(sql`CURRENT_TIMESTAMP`)
-            .notNull(),
-        updatedAt: timestamp('updated_at', { withTimezone: true }).$onUpdate(
-            () => new Date()
-        )
-    },
-    example => ({
-        createdByIdIdx: index('created_by_idx').on(example.createdById),
-        nameIndex: index('name_idx').on(example.name)
-    })
-)
-
 export const usersRelations = relations(users, ({ many }) => ({
     accounts: many(accounts)
 }))
